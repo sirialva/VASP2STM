bdafbba046b334b38aa056c4b548851132d657e2
Siri Berge <sb450@st-andrews.ac.uk>
2024-07-22 14:35:57 +0100

    trying CHG file instead

--- VASP2STM.py
+++ VASP2STM.py
@@ -13,8 +13,9 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from ase.calculators.vasp import VaspChargeDensity
 
-# read CHGCAR
-vasp_charge = VaspChargeDensity('CHGCAR')
+# read CHGCAR, charge density data
+vasp_charge = VaspChargeDensity('CHG')
 density = vasp_charge.chg[-1]
 atoms = vasp_charge.atoms[-1]
 del vasp_charge
+
