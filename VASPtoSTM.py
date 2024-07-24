388b56ed7659707c1b920d43a120e4a4a2acd4ac
Siri Berge <sb450@st-andrews.ac.uk>
2024-07-24 10:46:01 +0100

    added scan_mode

--- VASPtoSTM.py
+++ VASPtoSTM.py
@@ -33,25 +33,33 @@ class charge_density():
         self.unit_cell = self.atoms.get_cell()
         self.cell_lengths = np.sqrt(np.dot(self.unit_cell,self.unit_cell.transpose()).diagonal())
 
-    def make_cut(self, tip_height,
-              scan_mode='2D_slice',
-              cut_direction='c',
-              supercell=[1,1]):
+    def make_cut(self, 
+                 tip_height,
+                 scan_mode='2D_slice',
+                 cut_direction='c',
+                 supercell=[1,1]):
         self.tip_height = tip_height
-        
+
+        # Choose cut_direction, rearranges coordinate order
+        if cut_direction == 'c':
+            pass
+        elif cut_direction == 'b':
+            pass
+
+        # Find index of cut height slice
         cut_height_idx = int(round(tip_height/self.cell_lengths[2]*self.density.shape[2]))
-
-        print(cut_height_idx)
-
-        self.cut_density = self.density[:,:,cut_height_idx]
+        
+        # Find cut density
+        if scan_mode == '2D_slice':
+            self.cut_density = self.density[:,:,cut_height_idx]
+        elif scan_mode == 'constant_current':
+            self.cut_density = self.density[:,:,cut_height_idx]
     
     def plot(self,save_fig=False):
-        c = plt.matshow(self.cut_density, cmap='bone')
+        c = plt.matshow(self.cut_density, cmap='RdPu_r')
         plt.colorbar(c)
-        #plt.xticks(())
-        #plt.yticks(())
-        #plt.axis('square')
-        #plt.axis('off')
+        plt.xticks(())
+        plt.yticks(())
 
         if save_fig == True:
             plt.savefig(self.filename+'_'+str(round(self.tip_height,3))+'.pdf', dpi=300, bbox_inches='tight')
